d896d34ccb78828ca16baf31cdadfede5bd9d4f7
Kranthi Chalasani <kranthi.chalasani@gmail.com>
2013-10-10 23:45:31 -0700

    Added more collections

--- src/github.com/restly/db/models.go
+++ src/github.com/restly/db/models.go
@@ -65,6 +65,51 @@ type RestaurantBranch struct{
 	Zipcode string `bson:"z" json:"zipcode"`
 }
 
+type Item struct{
+	Id bson.ObjectId `bson:"_id,omitempty" json:"id"`
+	Name string	`bson:"n" json:"name"`
+	Desc string `bson:"d" json:"desc"`
+	Skus []Sku `bson:"s" json:"skus"`
+	CreateTime time.Time `bson:"ct" json:"created"`
+	UpdateTime time.Time `bson:"ut" json:"updated"`
+	
+	RestaurantKey string `bson:"-" json:"-"`
+}
+
+type Sku struct{
+	Price float64	`bson:"p" json:"price"`
+}
+
+type Category struct{
+	Id bson.ObjectId `bson:"_id,omitempty" json:"id"`
+	Name string	`bson:"n" json:"name"`
+	Desc string `bson:"d" json:"desc"`
+	CreateTime time.Time `bson:"ct" json:"created"`
+	UpdateTime time.Time `bson:"ut" json:"updated"`
+	
+	RestaurantKey string `bson:"-" json:"-"`
+}
+
+type Menu struct{
+	Id bson.ObjectId `bson:"_id,omitempty" json:"id"`
+	Name string `bson:"n" json:"name"` //lunch, dinner
+	Categories []MenuCategory `bson:"c" json:"categories"`
+	CreateTime time.Time `bson:"ct" json:"created"`
+	UpdateTime time.Time `bson:"ut" json:"updated"`
+	
+	RestaurantKey string `bson:"-" json:"-"`
+}
+
+type MenuCategory struct{
+	Category bson.ObjectId `bson:"id" json:"id"`
+	Items []CategoryItem `bson:"i" json:"items"`
+}
+
+type CategoryItem struct{
+	ItemId bson.ObjectId `bson:"id" json:"itemId"`
+	SkuIds []int `bson:"id" json:"skus"`
+}
+
 func (u *User) name() string{
 	return "user"
 }
@@ -154,6 +199,151 @@ func (r *Restaurant) Update() error{
 	c := session.DB(db).C(r.name())
 	return c.Update(bson.M{"_id":r.Id}, r)
 }
+
+func (i *Item) name() string{
+	return "item"
+}
+
+func (i *Item) valid() bool{
+	if i.Name == "" || i.RestaurantKey == ""{
+		return false
+	}
+	
+	return true
+}
+
+func (i *Item) Create() error{
+	if !i.valid(){
+		return missing_req
+	}
+	
+	if bson.ObjectId.Valid(i.Id){
+		return call_update
+	}
+	
+	session := m.session()
+	defer session.Close()
+	
+	i.CreateTime = time.Now()
+	i.UpdateTime = time.Now()
+	c := session.DB(i.RestaurantKey).C(i.name())
+	return c.Insert(&i)
+}
+
+func (i *Item) Update() error{
+	if !i.valid(){
+		return missing_req
+	}
+	
+	session := m.session()
+	defer session.Close()
+	
+	i.UpdateTime = time.Now()
+	c := session.DB(i.RestaurantKey).C(i.name())
+	return c.Update(bson.M{"_id": i.Id}, i)
+}
+
+
+func (c *Category) name() string{
+	return "category"
+}
+
+func (c *Category) valid() bool{
+
+	if c.Name == "" || c.RestaurantKey == ""{
+		return false
+	}
+	
+	return true
+}
+
+func (c *Category) Create() error{
+	if !c.valid(){
+		return missing_req
+	}
+	
+	if bson.ObjectId.Valid(c.Id){
+		return call_update
+	}
+	
+	session := m.session()
+	defer session.Close()
+	
+	c.CreateTime = time.Now()
+	c.UpdateTime = time.Now()
+	collection := session.DB(c.RestaurantKey).C(c.name())
+	return collection.Insert(&c)
+}
+
+func (c *Category) Update() error{
+	if !c.valid(){
+		return missing_req
+	}
+	
+	session := m.session()
+	defer session.Close()
+	
+	c.UpdateTime = time.Now()
+	collection := session.DB(c.RestaurantKey).C(c.name())
+	return collection.Update(bson.M{"_id": c.Id}, c)
+}
+
+func (m *Menu) name() string{
+	return "menu"
+}
+
+func (m *Menu) valid() bool{
+
+	if m.RestaurantKey == "" || m.Categories == nil || len(m.Categories) == 0{
+		return false
+	}
+	
+	for _,cat := range(m.Categories){
+		if !bson.ObjectId.Valid(cat.Category) || cat.Items == nil || len(cat.Items) == 0{
+			return false
+		}
+		
+		for _,itm := range(cat.Items){
+			if !bson.ObjectId.Valid(itm.ItemId) || itm.SkuIds == nil || len(itm.SkuIds) == 0{
+				return false
+			}
+		}
+	}
+	
+	return true
+}
+
+func (menu *Menu) Create() error{
+	if !menu.valid(){
+		return missing_req
+	}
+	
+	if bson.ObjectId.Valid(menu.Id){
+		return call_update
+	}
+	
+	session := m.session()
+	defer session.Close()
+	
+	menu.CreateTime = time.Now()
+	menu.UpdateTime = time.Now()
+	collection := session.DB(menu.RestaurantKey).C(menu.name())
+	return collection.Insert(&menu)
+}
+
+func (menu *Menu) Update() error{
+	if !menu.valid(){
+		return missing_req
+	}
+	
+	session := m.session()
+	defer session.Close()
+	
+	menu.UpdateTime = time.Now()
+	collection := session.DB(menu.RestaurantKey).C(menu.name())
+	return collection.Update(bson.M{"_id": menu.Id}, menu)
+}
+
 
 
 func UserByEmail(email string) (user *User, err error){
